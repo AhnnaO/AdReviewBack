44d85c5e4526e54ec55e17a94b0099b8ce8b6acb
Ahnna Östling <ahnnaostling@gmail.com>
2019-12-28 23:07:46 +0100

    read clientABC in JSON format

--- clients/clientABC/scandir.php
+++ clients/clientABC/scandir.php
@@ -1,15 +1,16 @@
 <?php
 
 $campaigns = "./";
+$scanned_directory = array_diff(scandir($campaigns), array('..', '.', 'scandir.php', '.DS_Store'));
+print_r(json_encode($scanned_directory));
 
-
-// $scanned_directory = array_diff(scandir($campaigns), array('..', '.', 'scandir.php', '.DS_Store'));
-// print_r($scanned_directory);
-
+//     foreach (scandir($campaigns) as $scanned_directory)
 //    $fl = scandir($campaigns);
 //    foreach (scandir($campaigns) as $fl)
 //    echo $fl."<br>";
-   $files = preg_grep("/^(\d+)?\.\d+$/", scandir($campaigns));
-   foreach (scandir($campaigns) as $files)
-   echo $files;
+//    $files = preg_grep("/^(\d+)?\.\d+$/", scandir($campaigns));
+//    foreach (scandir($campaigns) as $files)
+//    echo json_encode($files);
+//    
+//    echo $files;
 ?>
